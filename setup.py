542d7e97a3c03d3fc0bae50a91af972b23ac185e
Rex McKinnon <rex@mckinnon.ninja>
2022-01-25 21:52:10 -0800

    Added LICENSE

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ plugin_author_email = "rex@mckinnon.ninja"
 plugin_url = "https://github.com/Rex--/OctoPrint-Tentacles"
 
 # The plugin's license. Can be overwritten within OctoPrint's internal data via __plugin_license__ in the plugin module
-plugin_license = ""
+plugin_license = "BSD-3-Clause"
 
 # Any additional requirements besides OctoPrint should be listed here
 plugin_requires = []
